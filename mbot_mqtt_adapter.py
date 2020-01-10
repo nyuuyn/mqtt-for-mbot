4c9c5a457f7d2a56b3c2498a3c0fde2a54c58024
Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
2020-01-10 16:22:10 +0100

    adds handling of arguments to adapter
    Signed-off-by: Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>

--- mbot_mqtt_adapter.py
+++ mbot_mqtt_adapter.py
@@ -1,5 +1,6 @@
 from lib.mBot import *
 import paho.mqtt.client as mqtt
+import sys
 
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code "+str(rc))
@@ -30,18 +31,38 @@ def onDistance(value):
 
 sensortimeout = 2.0
 commandtimeout = 0.5
+mqtt_host = "localhost"
+mqtt_port = 1883
+mqtt_keepalive = 60
+usb_serial = "/dev/ttyUSB0"
 
 client = mqtt.Client()
 bot = mBot()
 
-if __name__ == '__main__':
+def substring_after(s, substring):
+	return s.partition(substring)[2].strip()
+
+def parse_arg(argv):
+	for arg in argv:
+		if str(arg).startswith("mqtt_host="):
+			if substring_after(arg,"mqtt_host="):
+				mqtt_host = substring_after(arg,"mqtt_host=")
+		if str(arg).startswith("mqtt_port="):
+			if substring_after(arg,"mqtt_port="):
+				mqtt_port = substring_after(arg,"mqtt_port=")
+		if str(arg).startswith("usb_serial="):
+			if substring_after(arg,"usb_serial="):
+				usb_serial = substring_after(arg,"usb_serial=")
+
+def main(argv):
+	parse_arg(argv)
 	client.on_connect=on_connect
 	client.on_message=on_message
-	client.connect("localhost",1883,60)
+	client.connect(mqtt_host,mqtt_port,mqtt_keepalive)
 	client.loop_start()
 	client.subscribe("mbot/command")
 
-	bot.startWithSerial("/dev/ttyUSB0")
+	bot.startWithSerial(usb_serial)
 	#bot.startWithHID()
 	while(1):
 		bot.requestLightOnBoard(1,onLight)
@@ -53,3 +74,6 @@ if __name__ == '__main__':
 		#sleep(5)
 		#bot.doMove(0,0)
 		#sleep(5)
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
